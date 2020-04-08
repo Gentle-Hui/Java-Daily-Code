39735d5185b6a3f637d82825444bfc3119061056
Gentle-Hui <1120365945@qq.com>
2020-04-08 22:38:54 +0800

    upload

---
+++ JavaSE_Code/src/wrh2020_4_8/ByteTest.java
@@ -0,0 +1,21 @@
+package wrh2020_4_8;
+
+public class ByteTest {
+    public static void main(String[] args) {
+        long a=10L;//长整型
+
+        System.out.println("a的值是："+a);
+        System.out.println("最大值是："+Long.MAX_VALUE);
+        System.out.println("最小值是："+Long.MIN_VALUE);
+        
+
+        int b=10;
+        int c=20;
+        System.out.println(b+c);
+        System.out.println("hhh"+b+c);
+    }
+
+
+
+    
+}
