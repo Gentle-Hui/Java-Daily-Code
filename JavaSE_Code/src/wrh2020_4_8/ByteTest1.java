ffe4d0975a3d3d4defd979c76bd5e9fcbaa84380
Gentle-Hui <1120365945@qq.com>
2020-04-08 22:49:13 +0800

    upload

---
+++ JavaSE_Code/src/wrh2020_4_8/ByteTest1.java
@@ -0,0 +1,12 @@
+package wrh2020_4_8;
+
+public class ByteTest1 {
+    public static void main(String[] args) {
+        for(int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+
+        }
+        System.out.println("开心");
+    }
+
+}
