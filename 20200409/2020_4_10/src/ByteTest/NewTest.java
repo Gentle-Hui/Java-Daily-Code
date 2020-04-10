10d6818d40d1eae96be1cab224c63b040969994f
Gentle-Hui <1120365945@qq.com>
2020-04-10 22:43:08 +0800

    upload

--- 20200409/2020_4_10/src/ByteTest/NewTest.java
+++ 20200409/2020_4_10/src/ByteTest/NewTest.java
@@ -1,4 +1,13 @@
 package ByteTest;
 
 public class NewTest {
+    public static void main(String[] args) {
+        for(int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+        System.out.println("开心");
+
+    }
+
+
 }
