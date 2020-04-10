77fc0ff236d406ff28a96a1f605871c0523a4f1a
Gentle-Hui <1120365945@qq.com>
2020-04-10 22:53:45 +0800

    upload

--- 20200409/2020_4_10/src/ByteTest/NewTest.java
+++ 20200409/2020_4_10/src/ByteTest/NewTest.java
@@ -1,11 +1,24 @@
 package ByteTest;
 
 public class NewTest {
+
+
+
     public static void main(String[] args) {
         for(int i=0;i<args.length;i++){
             System.out.println(args[i]);
         }
         System.out.println("开心");
+
+
+        byte b=12;
+        byte c=21;
+        System.out.println(b+" "+c);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+        //byte d= Byte.MAX_VALUE+1;
+        System.out.println(Byte.MAX_VALUE+1);
 
     }
 
