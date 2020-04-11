5d489b1f1719b721f5c26d248b71b65a2f735c3f
Gentle-Hui <1120365945@qq.com>
2020-04-11 09:27:04 +0800

    upload

--- JavaCode/2020_4_11/src/ByteTest/NewTest.java
+++ JavaCode/2020_4_11/src/ByteTest/NewTest.java
@@ -1,22 +1,18 @@
 package ByteTest;
 
-public class NewTest {
+public class NewTest{
 
+    public static void main(String[] args) {
+        int x = 0;
+        while(x<3)
+        {
+            x++;
+        }
+        System.out.println("x="+x);
 
-
-    int x= 0;
-		while (x<3)
-    {
-        x++;
+        for(int y = 0;y<3;y++){
+            System.out.println("y=" + y);
     }
-		System.out.println("x="+x);
 
 
-
-
-
-		for (int y=0;y<3 ;y++ )
-    {
-        System.out.println("y="+y);
-    }
 }
