29ffaf586ef98ff3d105ddd1ba1758d2fa6274f8
Gentle-Hui <1120365945@qq.com>
2020-04-11 09:20:15 +0800

    upload

---
+++ JavaCode/2020_4_11/src/ByteTest/NewTest.java
@@ -0,0 +1,22 @@
+package ByteTest;
+
+public class NewTest {
+
+
+
+    int x= 0;
+		while (x<3)
+    {
+        x++;
+    }
+		System.out.println("x="+x);
+
+
+
+
+
+		for (int y=0;y<3 ;y++ )
+    {
+        System.out.println("y="+y);
+    }
+}
