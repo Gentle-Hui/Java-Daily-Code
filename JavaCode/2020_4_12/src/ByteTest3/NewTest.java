e8357679a10005ebb4361785f6066bc884872983
Gentle-Hui <1120365945@qq.com>
2020-04-13 17:36:54 +0800

    upload

--- JavaCode/2020_4_12/src/ByteTest3/NewTest.java
+++ JavaCode/2020_4_12/src/ByteTest3/NewTest.java
@@ -1,4 +1,27 @@
 package ByteTest3;
 
+import java.util.Scanner;
+
 public class NewTest {
+    public static void main(String[] args) {
+        System.out.println("请输入你要判断的数字：");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        Prime(num);
+
+    }
+
+    private static void Prime(int num) {
+        int b = 0;
+        for (b = 2; b < num; b++) {
+            if (num % b == 0) {
+                System.out.println("不是素数");
+                break;
+            }
+        }
+        if (b >= num) {
+            System.out.println("是素数");
+        }
+
+    }
 }
