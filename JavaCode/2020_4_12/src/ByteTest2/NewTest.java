15240c965746eac491044a75e21b65450be821d3
Gentle-Hui <1120365945@qq.com>
2020-04-13 10:34:40 +0800

    upload

--- JavaCode/2020_4_12/src/ByteTest2/NewTest.java
+++ JavaCode/2020_4_12/src/ByteTest2/NewTest.java
@@ -1,4 +1,23 @@
 package ByteTest2;
 
 public class NewTest {
+    public static void main(String[] args) {
+        int a;
+        for (a = 1; a <= 100; a++) {
+            Prime(a);
+        }
+    }
+    private static void Prime(int a) {
+        int b = 0;
+        for (b = 2; b < a; b++) {
+            if (a % b == 0) {
+                break;
+            }
+        }
+        if (b >= a) {
+            System.out.println(a);
+        }
+    }
+
+
 }
