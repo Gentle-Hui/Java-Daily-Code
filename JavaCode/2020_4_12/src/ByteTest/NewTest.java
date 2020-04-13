c7b64a2cf525aefdf50f469ef2099a9e2e825dda
Gentle-Hui <1120365945@qq.com>
2020-04-13 09:45:07 +0800

    upload

--- JavaCode/2020_4_12/src/ByteTest/NewTest.java
+++ JavaCode/2020_4_12/src/ByteTest/NewTest.java
@@ -1,10 +1,21 @@
 package ByteTest;
-import java.util.Scanner;
+
 public class NewTest {
-    Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
-
-
-
-
+    public static void main(String[] args) {
+        GetChar();
+    }
+    private static void GetChar() {
+        int count=0;
+        for(int i=1;i<=100;i++){
+            if(i/10==9){
+                count++;
+                System.out.println(i);
+            }else if(i%10==9){
+               count++;
+                System.out.println(i);
+            }
+        }
+        count=count+1;
+        System.out.println("一共出现"+count+"次九");
+    }
 }
