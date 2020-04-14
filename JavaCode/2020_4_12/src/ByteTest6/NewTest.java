5030043e1e2b3ceae532dff61c9586b31965a3a4
Gentle-Hui <1120365945@qq.com>
2020-04-14 21:46:57 +0800

    upload

--- JavaCode/2020_4_12/src/ByteTest6/NewTest.java
+++ JavaCode/2020_4_12/src/ByteTest6/NewTest.java
@@ -2,7 +2,11 @@ package ByteTest6;
 
 public class NewTest {
     public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 5, 6, 7, 8, 9};
+        Find();
+    }
 
+    private static void Find() {
 
     }
 
