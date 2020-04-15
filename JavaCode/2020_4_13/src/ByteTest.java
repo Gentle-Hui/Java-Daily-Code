cd9570290a3ea7aa2cd379a7e189269b9c0f7355
Gentle-Hui <1120365945@qq.com>
2020-04-15 22:25:02 +0800

    upload

--- JavaCode/2020_4_13/src/ByteTest.java
+++ JavaCode/2020_4_13/src/ByteTest.java
@@ -1,14 +1,14 @@
 public class ByteTest {
     public static void main(String[] args) {
-        int[] array={1,2,3,4,5};
-        for(int i=0;i<array.length;i++){
+        int[] array = {1, 2, 3, 4, 5};
+        for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
 
         }
+        System.out.println("=================");
+        for (int val : array) {
 
-        for(int val : array){
-
-            System.out.println(val+" ");
+            System.out.println(val + " ");
         }
     }
 
