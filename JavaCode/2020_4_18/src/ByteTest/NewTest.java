5ab1f13af1e14f2ab1426ad3e76eebf06bae995c
Gentle-Hui <1120365945@qq.com>
2020-04-18 22:48:40 +0800

    upload

--- JavaCode/2020_4_18/src/ByteTest/NewTest.java
+++ JavaCode/2020_4_18/src/ByteTest/NewTest.java
@@ -1,6 +1,16 @@
 package ByteTest;
 
 public class NewTest {
+    private int i=1;
 
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public void setI(int a){
+        i=a;
+    }
 
 }
