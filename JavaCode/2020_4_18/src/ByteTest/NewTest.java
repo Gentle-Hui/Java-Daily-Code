fa0caf613a42133dfba8c3a0884d0bfd516f6e73
Gentle-Hui <1120365945@qq.com>
2020-04-19 15:40:58 +0800

    upload

--- JavaCode/2020_4_18/src/ByteTest/NewTest.java
+++ JavaCode/2020_4_18/src/ByteTest/NewTest.java
@@ -1,19 +1,28 @@
 package ByteTest;
 
+
 public class NewTest {
-    private int i = 1;
-
     public static void main(String[] args) {
-
+        double ret = 0.0;
+        double n = 0;
+        double r = 1;
+        for (int i = 1; i <= 100; i++) {
+            n = Math.pow(-1, i - 1);
+            ret += n * (r / i);
+        }
+        System.out.println(ret);
 
     }
 
-    public void setI(int a) {
-        i = a;
-    }
-
-    public int getI() {
-        return i;
+    public static void main1(String[] args) {
+        //计算1/1-1/2+1/3-1/4+1/5 …… + 1/99 - 1/100 的值
+        double num = 0.0;
+        double n = 1;
+        for (double i = 1; i <= 100; i++) {
+            num += n / i;
+            n = -n;
+        }
+        System.out.println(num);
     }
 
 }
