9f7839ed47b7d651af7135a16be429f7392dc0a7
Gentle-Hui <1120365945@qq.com>
2020-04-22 23:08:51 +0800

    upload

--- JavaCode/2020_4_20/src/HomeWork/NewTest.java
+++ JavaCode/2020_4_20/src/HomeWork/NewTest.java
@@ -7,4 +7,13 @@ package HomeWork;
  * @create: 2020-04-20 10:31
  **/
 public class NewTest {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,6,7,8,1,2,3,6,7};
+        search(arr);
+    }
+
+    private static void search(int[] arr) {
+
+
+    }
 }
