0bd7125d178fa70185049e168c630596f480b3e3
Gentle-Hui <1120365945@qq.com>
2020-04-23 23:19:06 +0800

    upload

--- JavaCode/2020_4_22/src/ByteTest/LinkedList.java
+++ JavaCode/2020_4_22/src/ByteTest/LinkedList.java
@@ -1,5 +1,7 @@
 package ByteTest;
 
+import java.util.SplittableRandom;
+
 /**
  * @program: 20200419
  * @description
@@ -8,58 +10,78 @@ package ByteTest;
  **/
 
 
-class Node{
+class Node {
     //定义单链表节点的两个部分data和next的对象
     public int data;
     public Node next;
+
     //构造方法
-    public Node(int data,Node next){
-        this.data=data;
-        this.next=next;
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
     }
 
 
 }
+
 public class LinkedList {
     //保存单链表的头节点的引用
     public Node head;
+
+
     //头插法
-    public void addFirst(int data){
-
+    public void addFirst(int data) {
+        Node node = new Node(data);//这句代码就实现了一个不确定地址，next为空，存在数据的节点
+        if(this.head==null){
+            //如果第一次插入节点
+            this.head=node;
+            return;
+        }
+        node.next = this.head;
+        this.head = node;
     }
+
     //尾插法
-    public void addLast(int data){
+    public void addLast(int data) {
+        Node node=new Node(data);
 
     }
+
     //任意位置插入，第一个节点为0号下表
-    public boolean addIndex(int index,int data){
+    public boolean addIndex(int index, int data) {
 
         return true;
     }
+
     //查找是否包含关键字key是否在单链表中
-    public boolean contains(int key){
+    public boolean contains(int key) {
 
         return true;
     }
+
     //删除第一次出现关键字key的节点
-    public void remove(int key){
+    public void remove(int key) {
 
     }
+
     //删除所有值为key的节点
-    public void removeAllKey(int key){
+    public void removeAllKey(int key) {
 
     }
+
     //单链表的长度
-    public int size(){
+    public int size() {
 
         return 0;
     }
+
     //遍历
-    public void display(){
+    public void display() {
 
     }
+
     //清空
-    public void clear(){
+    public void clear() {
 
     }
 
